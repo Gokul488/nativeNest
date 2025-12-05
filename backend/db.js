b038c136e044f13ef7ddaafb66b6722acbc1e26a
Gokul488 <gokul042000@gmail.com>
2025-12-05 18:27:21 +0530

    ca file added in db file

--- backend/db.js
+++ backend/db.js
@@ -1,16 +1,17 @@
+const mysql = require('mysql2/promise');
 
-     const mysql = require('mysql2/promise');
+let ca = undefined;
+if (process.env.AIVEN_CA_BASE64) {
+  ca = Buffer.from(process.env.AIVEN_CA_BASE64, 'base64').toString('utf8');
+}
 
-     const pool = mysql.createPool({
-       host: process.env.DB_HOST || 'localhost',
-       user: process.env.DB_USER || 'root',
-       password: process.env.DB_PASSWORD || '',
-       database: process.env.DB_NAME || 'mydb',
-       ssl: {
-            rejectUnauthorized: true   // Required by Aiven
-            },
-       waitForConnections: true,
-       connectionLimit: 10
-     });
-
-     module.exports = pool;
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  ssl: ca ? { ca, rejectUnauthorized: true } : false,
+  waitForConnections: true,
+  connectionLimit: 10,
+});
